62a08f1ff38935091fe812cf632e28548bf50b37
Greg Cain <codegregcode@gmail.com>
2023-02-15 18:28:49 +0000

    adds mocha writes src app controllers and routes for artist and sets up test environment

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const artistRouter = require('./routes/artist');
+app.use(express.json());
+
+app.use('/artists', artistRouter);
+
+module.exports = app;
