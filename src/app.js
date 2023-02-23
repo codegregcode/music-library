11e4d24db98bbf660222fefaa14e6d7dc304438b
Greg Cain <codegregcode@gmail.com>
2023-02-23 14:10:23 +0000

    adds hello bear

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ const app = express();
 const artistRouter = require('./routes/artist');
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World!ʕ•́ᴥ•̀ʔっ');
+});
+
 app.use('/artists', artistRouter);
 
 module.exports = app;
