7341f19ca5fa8f58121bd2c0f1838fbcc13fc692
Greg Cain <codegregcode@gmail.com>
2023-02-23 16:25:56 +0000

    creates new tests and controllers to update artist put/patch

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -23,4 +23,30 @@ const createArtist = async (req, res) => {
   }
 };
 
-module.exports = { createArtist, artistController };
+const getArtists = async (req, res) => {
+  try {
+    const { rows } = await db.query('SELECT * FROM Artists');
+    res.status(200).json(rows);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+const getArtist = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      rows: [artist],
+    } = await db.query('SELECT * FROM Artists WHERE id = $1', [id]);
+
+    if (!artist) {
+      return res.status(404).json({ message: `artist ${id} does not exist` });
+    }
+
+    res.status(200).json(artist);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+module.exports = { createArtist, getArtists, getArtist, artistController };
--- src/routes/artist.js
+++ src/routes/artist.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const artistController = require('../controllers/artist');
+const artistUpdater = require('../controllers/artist-update');
 
 const router = express.Router();
 
 router.post('/', artistController.createArtist);
+router.get('/', artistController.getArtists);
+router.get('/:id', artistController.getArtist);
+router.put('/:id', artistUpdater.putArtist);
+router.patch('/:id', artistUpdater.patchArtist);
 
 module.exports = router;
