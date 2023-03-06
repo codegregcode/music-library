8066d363f11e2271e5f336c0780cdd5f6eeaa85c
Greg Cain <codegregcode@gmail.com>
2023-03-06 21:28:08 +0000

    modifies app and creates routes for albums

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const artistRouter = require('./routes/artist');
+const albumRouter = require('./routes/album');
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -8,5 +9,6 @@ app.get('/', (req, res) => {
 });
 
 app.use('/artists', artistRouter);
+app.use('/albums', albumRouter);
 
 module.exports = app;
