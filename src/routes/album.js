55d63b9435e415d6244ebcd1d576542efe6d0041
Greg Cain <codegregcode@gmail.com>
2023-03-08 16:00:12 +0000

    deletes unused routes, rewrites post route

--- src/routes/album.js
+++ src/routes/album.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const albumController = require('../controllers/album');
-const albumUpdater = require('../controllers/album-update');
+// const albumUpdater = require('../controllers/album-update');
 
 const router = express.Router();
 
-router.post('/artist/:id/albums/', albumController.createAlbum);
-router.get('/albums/', albumController.getArtists);
-router.get('/albums/:albumId', albumController.getArtist);
-router.patch('/albums/:albumId', albumUpdater.patchArtist);
-router.delete('/albums/:albumId', albumController.deleteArtist);
+router.post('/:id/albums/', albumController.createAlbum);
+// router.get('/albums/', albumController.getAlbums);
+// router.get('/albums/:albumId', albumController.getArtist);
+// router.patch('/albums/:albumId', albumUpdater.patchArtist);
+// router.delete('/albums/:albumId', albumController.deleteArtist);
 
 module.exports = router;
