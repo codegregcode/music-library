72bc2147279b37379710fc3ff693548e5480f221
Greg Cain <codegregcode@gmail.com>
2023-03-08 15:58:10 +0000

    adds truncate cascade for albums

--- tests/helper.js
+++ tests/helper.js
@@ -1,5 +1,6 @@
 const db = require('../src/db');
 
 afterEach(async () => {
-  await db.query('TRUNCATE Artists CASCADE');
+  await db.query('TRUNCATE Artists CASCADE'),
+    await db.query('TRUNCATE Albums CASCADE');
 });
