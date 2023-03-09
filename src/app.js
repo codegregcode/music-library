bb8fe56dbe09882f4863bcdefa01e712cd0087ad
Greg Cain <codegregcode@gmail.com>
2023-03-09 22:51:09 +0000

    adds swagger to node and imports into app creates UI for artists

--- src/app.js
+++ src/app.js
@@ -2,7 +2,25 @@ const express = require('express');
 const app = express();
 const artistRouter = require('./routes/artist');
 const albumRouter = require('./routes/album');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const path = require('path');
+
 app.use(express.json());
+
+// ------ Configure swagger docs ------
+const options = {
+  swaggerDefinition: {
+    info: {
+      title: 'Music Library',
+      version: '1.0.0',
+      description: 'My API for adding Artists and Albums into a SQL database!',
+    },
+  },
+  apis: [path.join(__dirname, '/routes/*.js')],
+};
+
+const swaggerSpecs = swaggerJsdoc(options);
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello World!ʕ•́ᴥ•̀ʔっ');
@@ -11,5 +29,6 @@ app.get('/', (req, res) => {
 app.use('/artists', artistRouter);
 app.use('/artists', albumRouter);
 app.use('/albums', albumRouter);
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 
 module.exports = app;
--- src/routes/artist.js
+++ src/routes/artist.js
@@ -4,11 +4,167 @@ const artistUpdater = require('../controllers/artist-update');
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /artists:
+ *  post:
+ *      tags:
+ *          - Artists
+ *      description: Enters new artist into database
+ *      parameters:
+ *        - in: body
+ *          name: artist
+ *          description: Artist to enter into database
+ *          schema:
+ *              type: object
+ *              required:
+ *                - name
+ *                - genre
+ *              properties:
+ *                  name:
+ *                      type: string
+ *                  genre:
+ *                      type: string
+ *              example:
+ *                  name: Ornette Coleman
+ *                  genre: Jazz
+ *      responses:
+ *          201:
+ *              description: Artist successfully created
+ */
 router.post('/', artistController.createArtist);
+
+/**
+ * @swagger
+ * /artists:
+ *  get:
+ *      tags:
+ *          - Artists
+ *      description: Returns all artist records in database.
+ *      responses:
+ *          200:
+ *              description: html content
+ */
 router.get('/', artistController.getArtists);
+
+/**
+ * @swagger
+ * /artists/{id}:
+ *  get:
+ *      tags:
+ *          - Artists
+ *      description: Get/find an artist by ID
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          schema:
+ *              type: number
+ *          required: true
+ *          description: Numeric ID of artist to find
+ *      responses:
+ *          200:
+ *              description: id matches artist in database
+ *          404:
+ *              description: no artist found
+ */
 router.get('/:id', artistController.getArtist);
+
+/**
+ * @swagger
+ * /artists/{id}:
+ *  put:
+ *      tags:
+ *          - Artists
+ *      description: Update an artist by ID
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          schema:
+ *              type: integer
+ *          required: true
+ *          description: Numeric ID of the artist to update
+ *        - in: body
+ *          name: artist
+ *          description: The artist data to update
+ *          schema:
+ *              type: object
+ *              properties:
+ *                  name:
+ *                      type: string
+ *                  genre:
+ *                      type: string
+ *          required:
+ *            - name
+ *            - genre
+ *          example:
+ *            name: John Coltrane
+ *            genre: Jazz
+ *      responses:
+ *          200:
+ *              description: The updated artist
+ *          404:
+ *              description: The artist could not be found
+ */
 router.put('/:id', artistUpdater.putArtist);
+
+/**
+ * @swagger
+ * /artists/{id}:
+ *  patch:
+ *      tags:
+ *          - Artists
+ *      description: Update an artist by ID
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          schema:
+ *              type: integer
+ *          required: true
+ *          description: Numeric ID of the artist to update
+ *        - in: body
+ *          name: artist
+ *          description: The artist data to update
+ *          schema:
+ *              type: object
+ *              properties:
+ *                  name:
+ *                      type: string
+ *                  genre:
+ *                      type: string
+ *          required:
+ *            - name
+ *            - genre
+ *          example:
+ *            name: John Coltrane
+ *            genre: Jazz
+ *      responses:
+ *          200:
+ *              description: The updated artist
+ *          404:
+ *              description: The artist could not be found
+ */
 router.patch('/:id', artistUpdater.patchArtist);
+
+/**
+ * @swagger
+ * /artists/{id}:
+ *  delete:
+ *      tags:
+ *          - Artists
+ *      description: Deletes artist with matching ID
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          schema:
+ *              type: number
+ *          required: true
+ *          description: Numeric ID of artist to delete
+ *      responses:
+ *          200:
+ *              description: Artist successfully deleted
+ *          404:
+ *              description: Couldn't find artist to delete (check if ID is correct. If correct artist has albums which must be removed first)
+ */
 router.delete('/:id', artistController.deleteArtist);
 
 module.exports = router;
