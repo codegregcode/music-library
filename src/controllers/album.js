0a98b943f4f9fbf08b7d876e5deac489e9b8c583
Greg Cain <codegregcode@gmail.com>
2023-03-09 17:52:44 +0000

    removes commented out function, adds get and delete controllers

--- src/controllers/album.js
+++ src/controllers/album.js
@@ -1,12 +1,5 @@
 const db = require('../db/index');
 
-// const albumController = (req, res) => {
-//   res.status(201).json({
-//     name: req.body.name,
-//     date: req.body.date,
-//   });
-// };
-
 const createAlbum = async (req, res) => {
   const { name, date } = req.body;
   const { id } = req.params;
@@ -24,4 +17,47 @@ const createAlbum = async (req, res) => {
   }
 };
 
-module.exports = { createAlbum };
+const getAlbums = async (req, res) => {
+  try {
+    const { rows } = await db.query('SELECT * FROM Albums');
+    res.status(200).json(rows);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+const getAlbum = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      rows: [album],
+    } = await db.query('SELECT * FROM Albums WHERE id = $1', [id]);
+
+    if (!album) {
+      return res.status(404).json({ message: `album ${id} does not exist` });
+    }
+
+    res.status(200).json(album);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+const deleteAlbum = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const {
+      rows: [album],
+    } = await db.query(`DELETE FROM Albums WHERE id = $1 RETURNING *`, [id]);
+
+    if (!album) {
+      return res.status(404).json({ message: `album ${id} does not exist` });
+    }
+    res.status(200).json(album);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+module.exports = { createAlbum, getAlbums, getAlbum, deleteAlbum };
